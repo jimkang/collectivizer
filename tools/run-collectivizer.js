a5394dcd15a98b71cd3ddcf2731949b00a5f8076
Jim Kang <jimkang@gmail.com>
2015-05-05 10:24:17 -0400

    Removed debugger statement.

--- tools/run-collectivizer.js
+++ tools/run-collectivizer.js
@@ -15,7 +15,6 @@ var collectivizer = createCollectivizer({
 collectivizer.collectivize(cmdOpts[0], showResult);
 
 function showResult(error, result) {
-  debugger;
   if (error) {
     console.log(error);
   }
